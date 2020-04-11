a02f6e5ec982f7357fc60d128ab4659687631584
Gr <morgan.graeme@gmail.com>
2020-04-11 12:49:52 -0700

    Switch working

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Title from '../Title/Title';
 import GraphDraw from '../GraphDraw/GraphDraw';
@@ -10,6 +10,7 @@ import TokenNetwork from '../TokenNetwork/TokenNetwork';
 import Hourglass from '../Hourglass/Hourglass';
 import PingSim from '../PingSim/PingSim';
 import PingSame from '../PingSame/PingSame'
+import Switch from '../Switch/Switch';
 
 /**
  * This is the 'Driver' componenet for the rest of the app. 
@@ -20,9 +21,29 @@ import PingSame from '../PingSame/PingSame'
  * application. 
  */
 
-  class App extends React.Component{
 
-    render() {
+    function showStuff(value){
+      if(value === true){
+        return 'True';
+      }else{
+        return 'False';
+      }
+    }
+
+  //class App extends React.Component{
+    function App(){
+
+    // constructor(props){
+    //   super(props);
+
+    //   const [value, setValue] = useState(false);
+    // }
+    //render() {
+      //For Switch
+      //const [value, setValue] = useState(false);
+
+      const [value, setValue] = useState(true);
+
       return (
         <div>
 
@@ -32,6 +53,15 @@ import PingSame from '../PingSame/PingSame'
 
           <div className='flex-container wrap title'>
             <Title />
+          </div>
+
+
+          
+          <div className="flex-container wrap title">
+            <Switch 
+                isOn={value} 
+                handleToggle={() => setValue(!value)}
+            />
           </div>
 
           <div className='flex-container wrap ping_same'>
@@ -79,7 +109,7 @@ import PingSame from '../PingSame/PingSame'
       
         </div>
       )
-    }
+    //}
   }
 
 export default App;
